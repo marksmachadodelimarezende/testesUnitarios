9560cf2c607f3627b9bbdf3552589bf36ed23b61
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2021-12-11 21:24:35 -0300

    Rename to yml

--- springMVC/src/main/java/com/mmlrdev/springMVC/models/User.java
+++ springMVC/src/main/java/com/mmlrdev/springMVC/models/User.java
@@ -1,12 +1,16 @@
 package com.mmlrdev.springMVC.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Long id;
     private String name;
